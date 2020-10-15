47e80b40b326ebdcc026a69de5f81f65827054b1
Davi J <davijosantos.dj@gmail.com>
2020-10-15 15:10:08 -0300

    Happy Site

---
+++ public/scripts/page-orphanages.js
@@ -0,0 +1,40 @@
+// tipos de dados
+// String ""
+// Number 01
+// Object {}
+// Boolean True or False
+// Arrey []
+
+const mymap = L.map('mapid').setView([-5.076233, -42.818672], 15);
+
+// create and add tileLayer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',)
+.addTo(mymap);
+
+// create icon
+const icon = L.icon({
+    iconUrl: "./public/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
+
+// create popup overlay
+const popup = L.popup({
+    closeButton: false,
+    className: 'map-popup',
+    minWidth: 240,
+    minHeight: 240
+}).setContent('Lar de Maria <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"></a>')
+
+
+// create and add marker
+L
+.marker([-5.076233, -42.818672], { icon })
+.addTo(mymap)
+.bindPopup(popup)
+
+
+/*L.marker([-5.070094, -42.813739], { icon })
+.addTo(mymap)
+.bindPopup(popup)*/
